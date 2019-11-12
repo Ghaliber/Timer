01b08b36d73e8f8c10c9c5061a88422ea11bb193
Your Name <you@example.com>
2019-11-12 10:31:47 +0100

    Timer01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Timer from './Timer'
 import './App.css';
-
+const MilliSec = 8655000
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Timer time={MilliSec} />
     </div>
   );
 }
